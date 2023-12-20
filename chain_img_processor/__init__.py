10a67fb4339926cf6a0f24c7e926b07280e9f3b3
virtuanista <cpdts03@gmail.com>
2023-12-20 13:34:01 +0100

    ⭐⭐⭐⭐⭐

---
+++ chain_img_processor/__init__.py
@@ -0,0 +1,5 @@
+from .image import ChainImgProcessor, ChainImgPlugin, get_single_image_processor, version
+from .video_old import ChainVideoProcessor, get_single_video_processor
+from .video import ChainVideoImageProcessor
+from .batchimage import ChainBatchImageProcessor
+from .ffmpeg_writer import FFMPEG_VideoWriter
